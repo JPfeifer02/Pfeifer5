f6b3560fbb6a89d92ba869987e4425d05a835548
JPfeifer02 <john02pf@gmail.com>
2023-04-19 10:35:56 -0500

    Initial

---
+++ Pfeifer5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GraphNS;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new graph object
+        Graph graph = new Graph("graph.json");
+        // Perform depth-first search
+        Console.WriteLine("Performing depth-first search...");
+        graph.DepthFS(0);
+        Console.WriteLine("");
+
+        // Perform breadth-first search
+        Console.WriteLine("Performing breadth-first search...");
+        graph.BreadthFS(0);
+        Console.WriteLine("");
+
+        Console.ReadLine();
+
+    }
+}
